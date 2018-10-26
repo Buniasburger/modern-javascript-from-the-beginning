ea5b871901f3a0fc0368580eca90ededdd7014bd
Piotr Opozda <popozda@protonmail.com>
2018-10-27 01:10:07 +0200

    Add tasks filtering

--- task_list/app.js
+++ task_list/app.js
@@ -11,9 +11,13 @@ loadEventListeners();
 // Load all event listeners
 function loadEventListeners() {
     // Add task event
-    form.addEventListener('submit', addTask);
+    form.addEventListener('submit', e => addTask(e));
     // Remove task event
-    taskList.addEventListener('click', removeTask);
+    taskList.addEventListener('click', e => removeTask(e));
+    // Clear tasks event
+    clearBtn.addEventListener('click', () => clearTasks());
+    // Filter tasks event
+    filter.addEventListener('keyup', e => filterTasks(e));
 }
 
 // Add task
@@ -50,4 +54,25 @@ function removeTask(e) {
     if(e.target.parentElement.classList.contains('delete-item') && confirm('Are you sure?')) {
         e.target.parentElement.parentElement.remove();
     }
+}
+
+// Clear tasks
+function clearTasks() {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('li.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+
+        if (item.toLowerCase().indexOf(text) !== -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
