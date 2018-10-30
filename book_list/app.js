f017dba0c3f4a87dfaa515f9a7340928b15fb11c
Piotr Opozda <popozda@protonmail.com>
2018-10-30 04:11:12 +0100

    Working on UI Add items to list on submit Clear form inputs after submition

---
+++ book_list/app.js
@@ -0,0 +1,56 @@
+// Book Contructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {
+    //
+}
+
+UI.prototype.addBookToList = function (book) {
+    const list = document.querySelector('#book-list');
+    // Create tr element
+    const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+};
+
+UI.prototype.clearFields = function() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+}
+
+
+
+// Event listeners
+document.querySelector('#book-form').addEventListener('submit', function (e) {
+    // Get form values
+    const title = document.querySelector('#title').value,
+        author = document.querySelector('#author').value,
+        isbn = document.querySelector('#isbn').value;
+
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Clear fields
+    ui.clearFields();
+
+    e.preventDefault();
+});
