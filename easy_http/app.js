2c0297eb1ce1ec2bcb2b9be16793dc180014564d
Piotr Opozda <popozda@protonmail.com>
2018-11-02 04:46:33 +0100

    Create and test post method

--- easy_http/app.js
+++ easy_http/app.js
@@ -1,11 +1,24 @@
 const http = new EasyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(error, response) {
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(error, response) {
+//     if(error) {
+//         console.log(error)
+//     } else {
+//         console.log(response)
+//     }
+//
+// });
+
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+};
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function (error, response) {
     if(error) {
-        console.log(error)
+        console.log(response)
     } else {
         console.log(response)
     }
-
 });
--- easy_http/easyhttp.js
+++ easy_http/easyhttp.js
@@ -3,13 +3,13 @@ function EasyHTTP() {
 }
 
 // Make an HTTP GET Request
-EasyHTTP.prototype.get = function(url, callback) {
+EasyHTTP.prototype.get = function (url, callback) {
     this.http.open('GET', url, true);
 
     let self = this;
 
-    this.http.onload = () => {
-        if(self.http.status === 200) {
+    this.http.onload = function () {
+        if (self.http.status === 200) {
             callback(null, self.http.responseText);
         } else {
             callback('Error:' + self.http.status);
@@ -18,7 +18,20 @@ EasyHTTP.prototype.get = function(url, callback) {
 
     this.http.send();
 };
+
 // Make an HTTP POST Request
+EasyHTTP.prototype.post = function (url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+
+    this.http.onload = function () {
+        callback(null, self.http.responseText);
+    };
+
+    this.http.send(JSON.stringify(data));
+};
 
 // Make an HTTP PUT Request
 
