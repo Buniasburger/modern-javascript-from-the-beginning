90ff10cddc0c0e63b4c02aa657a864433f0fa6ef
Piotr Opozda <popozda@protonmail.com>
2018-11-03 16:22:36 +0100

    Fetching profile data on user input, inserting data info prepared and styled container

--- github_finder/github.js
+++ github_finder/github.js
@@ -5,7 +5,7 @@ class Github {
     }
 
     async getUser(user) {
-        const profile = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        const profile = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         return {
             profile: await profile.json()
