9515acde3951e87f858483d9e3bf417f17ed7f50
Piotr Opozda <popozda@protonmail.com>
2018-11-10 20:55:57 +0100

    Bind blur event listeners to form inputs, listeners callback are left empty for now

---
+++ form_validation/app.js
@@ -0,0 +1,18 @@
+// Form Blur Event Listeners
+document.querySelector('#name').addEventListener('blur', validateName);
+document.querySelector('#zip').addEventListener('blur', validateZip);
+document.querySelector('#email').addEventListener('blur', validateEmail);
+document.querySelector('#phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+    //
+}
+function validateZip() {
+    //
+}
+function validateEmail() {
+    //
+}
+function validatePhone() {
+    //
+}
