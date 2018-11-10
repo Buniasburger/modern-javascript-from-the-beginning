ee84cd2b1486f689cd506b34b7b793e4e59db66e
Piotr Opozda <popozda@protonmail.com>
2018-11-10 21:05:25 +0100

    Implement validateName callback

--- form_validation/app.js
+++ form_validation/app.js
@@ -4,11 +4,18 @@ document.querySelector('#zip').addEventListener('blur', validateZip);
 document.querySelector('#email').addEventListener('blur', validateEmail);
 document.querySelector('#phone').addEventListener('blur', validatePhone);
 
-function validateName() {
-    //
+function validateName(e) {
+    const name = e.target.value;
+    const re = /^[A-Za-z\s]{2,10}$/;
+
+    if(!re.test(name)) {
+        e.target.classList.add('is-invalid');
+    } else {
+        e.target.classList.remove('is-invalid');
+    }
 }
 function validateZip() {
-    //
+
 }
 function validateEmail() {
     //
